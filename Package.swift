0ff6d9b7577b662fc0d11c362db931b341f7138a
Leo Dion <leogdion@brightdigit.com>
2021-08-11 11:16:57 -0400

    first try

--- Package.swift
+++ Package.swift
@@ -1,26 +1,24 @@
-// swift-tools-version:5.2.4
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.2
 
 import PackageDescription
 
 let package = Package(
     name: "MailchimpKit",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "MailchimpKit",
-            targets: ["MailchimpKit"]),
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
     ],
-  dependencies: [      
-  ],
+    products: [
+        .library(name: "MailchimpKit", targets: ["MailchimpKit"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .exact("5.4.3")),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MailchimpKit",
-            dependencies: []),
-        .testTarget(
-            name: "MailchimpKitTests",
-            dependencies: ["MailchimpKit"]),
+        .target(name: "MailchimpKit", dependencies: [
+          "Alamofire",
+        ])
     ]
 )
