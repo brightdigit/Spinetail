135e1f7ed28b64c4ebf9d4aafa258b769f64e460
Leo Dion <leogdion@brightdigit.com>
2021-08-11 10:36:19 -0400

    working on using swaggen instead

---
+++ Tests/MailchimpKitTests/MailchimpKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MailchimpKit
+
+final class MailchimpKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        //XCTAssertEqual(MailchimpKit().text, "Hello, World!")
+    }
+}
