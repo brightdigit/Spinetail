8e1bbbd5ae2a23c37fbfa902dbedbc52323ba999
Leo Dion <leogdion@brightdigit.com>
2021-12-25 20:06:45 -0500

    adding Rocket

--- Package@swift-5.5.swift
+++ Package@swift-5.5.swift
@@ -18,6 +18,7 @@ let package = Package(
     .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.47.0"), // dev
     .package(url: "https://github.com/realm/SwiftLint", from: "0.43.0"), // dev
     .package(url: "https://github.com/brightdigit/swift-test-codecov", from: "1.0.0"), // dev
+    .package(url: "https://github.com/shibapm/Rocket", from: "1.2.0"), // dev
     .package(url: "https://github.com/brightdigit/Prch.git", from: "0.1.0")
   ],
   targets: [
