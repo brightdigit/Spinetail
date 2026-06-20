brightdigit/ButtondownKit

Phase 4 — OpenAPI & dependency migration (#109)

Package.swift
@@ -1,0 +1,66 @@
+// swift-tools-version:6.4
+// swiftlint:disable explicit_top_level_acl
+import PackageDescription
+
+let package = Package(
+  name: "ButtondownKit",
+  platforms: [
+    .macOS(.v13),
+    .iOS(.v16),
+    .tvOS(.v16),
+    .watchOS(.v9)
+  ],
+  products: [
+    .library(name: "ButtondownKit", targets: ["ButtondownKit"])
+  ],
+  dependencies: [
+    // NOTE: swift-openapi-generator is intentionally NOT a dependency here.
+    // The Types.swift/Client.swift under Sources/ButtondownKit/Generated are
+    // produced ahead of time by Scripts/generate-openapi-buttondown.sh (the
+    // generator is pinned in .mise.toml via the spm backend) and committed.
+    // The build-tool/command plugin is deliberately not used.
+    .package(
+      url: "https://github.com/apple/swift-openapi-runtime",
+      from: "1.8.0"
+    ),
+    // URLSessionTransport works on both Apple platforms and Linux
+    // (via FoundationNetworking), keeping the CI Linux container green.
+    .package(
+      url: "https://github.com/apple/swift-openapi-urlsession",
+      from: "1.0.0"
+    ),
+    // Transitive via swift-openapi-runtime; declared explicitly so the contract
+    // tests can name HTTPRequest/HTTPResponse in their mock transport.
+    .package(
+      url: "https://github.com/apple/swift-http-types",
+      from: "1.0.0"
+    )
+  ],
+  targets: [
+    .target(
+      name: "ButtondownKit",
+      dependencies: [
+        .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+        .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
+        .product(name: "HTTPTypes", package: "swift-http-types")
+      ],
+      // The vendored OpenAPI spec and generator config are inputs to
+      // Scripts/generate-openapi-buttondown.sh, not build resources.
+      exclude: [
+        "OpenAPI/openapi.json",
+        "OpenAPI/openapi-generator-config.yaml"
+      ]
+    ),
+    .testTarget(
+      name: "ButtondownKitTests",
+      dependencies: [
+        "ButtondownKit",
+        .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+        .product(name: "HTTPTypes", package: "swift-http-types")
+      ],
+      resources: [
+        .copy("Fixtures")
+      ]
+    )
+  ]
+)

brightdigit/Spinetail

Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:6.4
 // swiftlint:disable explicit_top_level_acl
 import PackageDescription
 
@@ -11,15 +11,61 @@ let package = Package(
     .watchOS(.v9)
   ],
   products: [
+    // The public product is the abstract `Spinetail` layer (async Mailchimp
+    // client, campaign DTO, auth middleware). It wraps the generated
+    // swift-openapi-generator client, which consumers never see directly.
     .library(name: "Spinetail", targets: ["Spinetail"])
   ],
   dependencies: [
-    .package(url: "https://github.com/brightdigit/Prch.git", from: "1.0.0-alpha.1")
+    .package(
+      url: "https://github.com/apple/swift-openapi-runtime",
+      from: "1.8.0"
+    ),
+    .package(
+      url: "https://github.com/apple/swift-openapi-urlsession",
+      from: "1.0.0"
+    ),
+    // Transitive via swift-openapi-runtime; declared explicitly so the
+    // contract tests can name HTTPRequest/HTTPResponse in their mock transport.
+    .package(
+      url: "https://github.com/apple/swift-http-types",
+      from: "1.0.0"
+    )
   ],
   targets: [
-    .target(name: "Spinetail", dependencies: [
-      .product(name: "PrchModel", package: "Prch")
-    ]),
-    .testTarget(name: "SpinetailTests", dependencies: ["Spinetail", "Prch"])
+    // GENERATED ONLY: Types.swift / Client.swift are generated ahead of time by
+    // Scripts/generate-openapi-spinetail.sh and committed (no build plugin).
+    // The generator config lives alongside the output but is not a Swift source.
+    .target(
+      name: "SpinetailOpenAPI",
+      dependencies: [
+        .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+        .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
+        .product(name: "HTTPTypes", package: "swift-http-types")
+      ],
+      exclude: [
+        "openapi-generator-config.yaml"
+      ]
+    ),
+    // Abstract layer: the hand-written async wrapper, campaign DTO, and auth
+    // middleware. The only target consumers depend on.
+    .target(
+      name: "Spinetail",
+      dependencies: [
+        "SpinetailOpenAPI",
+        .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+        .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
+        .product(name: "HTTPTypes", package: "swift-http-types")
+      ]
+    ),
+    .testTarget(
+      name: "SpinetailTests",
+      dependencies: [
+        "Spinetail",
+        "SpinetailOpenAPI",
+        .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+        .product(name: "HTTPTypes", package: "swift-http-types")
+      ]
+    )
   ]
 )